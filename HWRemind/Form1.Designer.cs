0a36c93c385c5a2fa37725884d4b7e28539eae85
Edward Webb <spider.pole@live.com>
2017-02-14 19:45:16 +1100

    First commit, uploading files

---
+++ HWRemind/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace HWRemind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.englishButton = new System.Windows.Forms.Button();
+            this.mathsButton = new System.Windows.Forms.Button();
+            this.physButton = new System.Windows.Forms.Button();
+            this.busButton = new System.Windows.Forms.Button();
+            this.sddButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // englishButton
+            // 
+            this.englishButton.BackColor = System.Drawing.Color.Red;
+            this.englishButton.ForeColor = System.Drawing.Color.Black;
+            this.englishButton.Location = new System.Drawing.Point(13, 14);
+            this.englishButton.Name = "englishButton";
+            this.englishButton.Size = new System.Drawing.Size(329, 185);
+            this.englishButton.TabIndex = 0;
+            this.englishButton.Text = "English";
+            this.englishButton.UseVisualStyleBackColor = false;
+            // 
+            // mathsButton
+            // 
+            this.mathsButton.BackColor = System.Drawing.Color.Lime;
+            this.mathsButton.Location = new System.Drawing.Point(13, 217);
+            this.mathsButton.Name = "mathsButton";
+            this.mathsButton.Size = new System.Drawing.Size(329, 185);
+            this.mathsButton.TabIndex = 1;
+            this.mathsButton.Text = "Maths";
+            this.mathsButton.UseVisualStyleBackColor = false;
+            // 
+            // physButton
+            // 
+            this.physButton.BackColor = System.Drawing.Color.Lime;
+            this.physButton.Location = new System.Drawing.Point(348, 14);
+            this.physButton.Name = "physButton";
+            this.physButton.Size = new System.Drawing.Size(329, 185);
+            this.physButton.TabIndex = 2;
+            this.physButton.Text = "Physics";
+            this.physButton.UseVisualStyleBackColor = false;
+            // 
+            // busButton
+            // 
+            this.busButton.BackColor = System.Drawing.Color.Lime;
+            this.busButton.Location = new System.Drawing.Point(348, 217);
+            this.busButton.Name = "busButton";
+            this.busButton.Size = new System.Drawing.Size(329, 185);
+            this.busButton.TabIndex = 3;
+            this.busButton.Text = "Business";
+            this.busButton.UseVisualStyleBackColor = false;
+            // 
+            // sddButton
+            // 
+            this.sddButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.sddButton.Location = new System.Drawing.Point(683, 14);
+            this.sddButton.Name = "sddButton";
+            this.sddButton.Size = new System.Drawing.Size(181, 388);
+            this.sddButton.TabIndex = 4;
+            this.sddButton.Text = "SDD";
+            this.sddButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 435);
+            this.Controls.Add(this.sddButton);
+            this.Controls.Add(this.busButton);
+            this.Controls.Add(this.physButton);
+            this.Controls.Add(this.mathsButton);
+            this.Controls.Add(this.englishButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button englishButton;
+        private System.Windows.Forms.Button mathsButton;
+        private System.Windows.Forms.Button physButton;
+        private System.Windows.Forms.Button busButton;
+        private System.Windows.Forms.Button sddButton;
+    }
+}
+
---
+++ HWRemind/ListItems.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HWRemind
+{
+    partial class ListItems
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subjectNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // subjectNameLabel
+            // 
+            this.subjectNameLabel.AutoSize = true;
+            this.subjectNameLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subjectNameLabel.Location = new System.Drawing.Point(23, 13);
+            this.subjectNameLabel.Name = "subjectNameLabel";
+            this.subjectNameLabel.Size = new System.Drawing.Size(152, 29);
+            this.subjectNameLabel.TabIndex = 0;
+            this.subjectNameLabel.Text = "Subject Name";
+            // 
+            // ListItems
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 236);
+            this.Controls.Add(this.subjectNameLabel);
+            this.Name = "ListItems";
+            this.Text = "ListItems";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label subjectNameLabel;
+    }
+}
